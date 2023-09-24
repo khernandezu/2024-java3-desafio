836a40b3c696af3cf4e1da85fe6bdef006eae4ff
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-09-23 23:03:05 -0300

    Video 5.3

--- src/Compra.java
+++ src/Compra.java
@@ -1,4 +1,4 @@
-public class Compra {
+public class Compra implements Comparable<Compra> {
     private double valor;
     private String descripcion;
 
@@ -19,5 +19,10 @@ public class Compra {
     public String toString() {
         return "Compra: valor=" + valor +
                 ", descripcion='" + descripcion;
+    }
+
+    @Override
+    public int compareTo(Compra otraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
     }
 }
